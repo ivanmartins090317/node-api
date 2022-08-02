3ff9f540860e099bf11a0a7e276485112324e850
Ivan Martins <ivanmartins.ilha@gmail.com>
2022-08-02 16:23:20 -0300

    cria hash de jwt e cria rota de login

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../routes/models/user");
-/* GET users listing. */
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.JWT_TOKEN;
 
+// ----ROTAS----
 router.post("/register", async (req, res) => {
-  console.log("OLA ESTOU AQUI");
   const { name, email, password } = req.body;
   const user = new User({ name, email, password });
 
@@ -13,6 +15,27 @@ router.post("/register", async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: "Error registering new user" });
+  }
+});
+
+router.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) res.status(401).json({ error: "Incorrect email or password" });
+    else {
+      user.isCorrectPassword(password, function (err, same) {
+        if (!same)
+          res.status(401).json({ error: "Incorrect email or password" });
+        else {
+          const token = jwt.sign({ email }, secret, { expiresIn: "10d" });
+          res.json({ user: user, token: token });
+        }
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ Erro: "Internal error, plese try again" });
   }
 });
 
